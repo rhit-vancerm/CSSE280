0848d109614109de877ca8b0b89d09bff97c6ac5
vancerm <vancerm@rose-hulman.edu>
2023-04-10 17:51:32 -0400

    nodemon install

--- FollowAlongs/Node/SimpleServers/noExpress.js
+++ FollowAlongs/Node/SimpleServers/noExpress.js
@@ -1,29 +1,68 @@
+let abcTracker = 0;
+let totalTracker = 0;
+const _ = require("underscore");
 const http = require('http');
 
-const mainHandler = function (request, response) {
+const abcHandler = (request, response) => {
+    response.write('<h1>ABC</h1>');
+};
 
-    if(request.url == "/favicon.ico"){
-        response.writeHead(204,{'Content-Type':'image/x-icon'});
-        response.end();
-        //console.log("favicon requested");
-        return;
-    }
-
-    console.log(request.url);
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/html');
+const xyzHandler = (request, response) => {
     response.write("<!doctype html>\n");
     response.write("<html>\n<head>\n");
     response.write("<title>Dice Roller</title>\n");
     response.write("</head>\n<body>\n");
     response.write('<h1>Hello World!</h1>');
+    response.write('<div>abcTracker = ' + abcTracker + '</div>');
+    response.write('<div>totalTracker = ' + totalTracker + '</div>');
+    for(let i = 0; i<5; i++){
+        let randNum = _.random(1,60);
+        response.write('<p>'+randNum+'</p>');
+    }
+
+
     response.write("\n</body>\n</html>");
+};
+
+const faviconHandler = (request, response) => {
+    response.writeHead(204, {
+        'Content-Type': 'image/x-icon'
+    });
+    response.end();
+
+    //console.log("favicon requested");
+};
+
+const mainHandler = function (request, response) {
+
+    if (request.url == "/favicon.ico") {
+        faviconHandler(request,response);
+        return;
+    }
+
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/html');
+
+    console.log(request.url);
+
+    if (request.url == "/abc") {
+        abcTracker++;
+        abcHandler(request,response);
+    }
+
+    let re =/^\/xyz.*/;
+
+    if (re.test(request.url)) {
+        xyzHandler(request,response);
+    }
+
+    totalTracker++;
     response.end();
 }
 
 const server = http.createServer(mainHandler);
 server.listen(3000, (err) => {
-    if(err) {
+    if (err) {
         console.log("Error", err);
     }
     console.log("listening on port 3000");
