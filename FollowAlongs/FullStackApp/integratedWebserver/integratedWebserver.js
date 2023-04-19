dc04e17f5a9a072203cf928a6e93477ca6a98f9b
vancerm <vancerm@rose-hulman.edu>
2023-04-18 23:16:47 -0400

    fullstack init

---
+++ FollowAlongs/FullStackApp/integratedWebserver/integratedWebserver.js
@@ -0,0 +1,48 @@
+var express = require("express");
+var app = express();
+
+let data=[];
+
+const logger = require("morgan");
+app.use(logger('dev')); //information serverside when requests come in
+
+const fs = require("fs");
+const serverSideStorage = "../data/db.json";
+
+
+fs.readFile(serverSideStorage, function(err, buf){
+    if(err){
+        console.log("error: ",err);
+    }
+    else{
+        data = JSON.parse(buf.toString());
+        if(data.length != 0){
+            counter = data[data.length-1];
+        }
+    }
+    console.log("data read from file.");
+});
+
+function saveToServer(data){
+    fs.writeFile(serverSideStorage, JSON.stringify(data), function(err,buf){
+        if(err){
+            console.log("error: ",err);
+        }
+        else{
+            console.log("data saved successfully!");
+        }
+    })
+}
+
+
+
+app.use('/static', express.static("public"));
+
+
+//middleware
+var bodyParser = require("body-parser");
+app.use('/api/', bodyParser.urlencoded({extended: true}));
+app.use('/api/', bodyParser.json());
+
+
+app.listen(3000);
